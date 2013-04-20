25715f113043425b39309d81994cc1f788d46a90
Kevin Hester <kevinh@geeksville.com>
2013-04-20 11:25:27 -0700

    rc channel localized

--- andropilot/src/main/scala/com/geeksville/andropilot/gui/RcChannelsFragment.scala
+++ andropilot/src/main/scala/com/geeksville/andropilot/gui/RcChannelsFragment.scala
@@ -10,8 +10,9 @@ import com.geeksville.util.ThreadTools._
 import android.support.v4.app.ListFragment
 import com.geeksville.andropilot.R
 import com.geeksville.andropilot.service._
+import com.ridemission.scandroid._
 
-class RcChannelsFragment extends SimpleListFragment {
+class RcChannelsFragment extends SimpleListFragment with UsesResources {
 
   override def onVehicleReceive = {
     case MsgRcChannelsChanged(_) =>
@@ -22,8 +23,10 @@ class RcChannelsFragment extends SimpleListFragment {
   }
 
   private def rcToSeq(m: msg_rc_channels_raw) =
-    Seq("Channel 1" -> m.chan1_raw, "Channel 2" -> m.chan2_raw, "Channel 3" -> m.chan3_raw, "Channel 4" -> m.chan4_raw,
-      "Channel 5" -> m.chan5_raw, "Channel 6" -> m.chan6_raw, "Channel 7" -> m.chan7_raw, "Channel 8" -> m.chan8_raw /* "Rssi" -> m.rssi */ )
+    Seq(m.chan1_raw,  m.chan2_raw, m.chan3_raw, m.chan4_raw,
+       m.chan5_raw, m.chan6_raw, m.chan7_raw, m.chan8_raw).zipWithIndex.map { case (a, i) =>
+         S(R.string.channel).format(i) -> a
+         }
 
   protected def makeAdapter(): Option[SimpleAdapter] = {
     for { v <- myVehicle; rc <- v.rcChannels } yield {
