94c1e70eb5f9cb3e4390ad5767c2a0c11dabc1bb
Kevin Hester <kevinh@geeksville.com>
2013-09-14 09:55:31 -1000

    better graph

--- common/compile-statemachine.sh
+++ common/compile-statemachine.sh
@@ -2,10 +2,10 @@ JAVABIN=/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 SMCJAR=~/packages/state-machine-compiler/bin/Smc.jar
 
 $JAVABIN -jar $SMCJAR -scala -d src/main/scala/com/geeksville/flight VehicleFSM.sm
-$JAVABIN -jar $SMCJAR -graph -glevel 1 VehicleFSM.sm
+$JAVABIN -jar $SMCJAR -graph -glevel 0 VehicleFSM.sm
 
 dot -Tsvg VehicleFSM.dot >VehicleFSM.svg
 dot -Tpng VehicleFSM.dot >VehicleFSM.png
 rm VehicleFSM.dot
 
-# gnome-open VehicleFSM.svg
+gnome-open VehicleFSM.svg
