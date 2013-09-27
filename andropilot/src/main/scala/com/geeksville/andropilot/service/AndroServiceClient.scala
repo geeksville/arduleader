b0f823f0b5d083e12f0d522c879bee9802f77953
Kevin Hester <kevinh@geeksville.com>
2013-09-26 16:42:39 -1000

    don't warn for minor things if the vehicle is gone

--- andropilot/src/main/scala/com/geeksville/andropilot/service/AndroServiceClient.scala
+++ andropilot/src/main/scala/com/geeksville/andropilot/service/AndroServiceClient.scala
@@ -28,18 +28,18 @@ trait AndroServiceClient extends AndroidLogger with AndropilotPrefs {
   private var myVListener: Option[MyVehicleListener] = None
   protected var service: Option[AndropilotService] = None
 
-  def isLowVolt = (for { v <- myVehicle; volt <- v.batteryVoltage } yield { volt < minVoltage }).getOrElse(false)
+  def isLowVolt = (for { v <- myVehicle; volt <- v.batteryVoltage } yield { v.hasHeartbeat && volt < minVoltage }).getOrElse(false)
 
   /// Apparently ardupane treats -1 for pct charge as 'no idea'
   def isLowBatPercent = (for { v <- myVehicle; pct <- v.batteryPercent } yield {
-    pct < minBatPercent && pct >= -1.0
+    v.hasHeartbeat && (pct < minBatPercent && pct >= -1.0)
   }).getOrElse(false)
   def isLowRssi = (for { v <- myVehicle; r <- v.radio } yield {
     val span = minRssiSpan
 
-    r.rssi - span < r.noise || r.remrssi - span < r.remnoise
+    v.hasHeartbeat && (r.rssi - span < r.noise || r.remrssi - span < r.remnoise)
   }).getOrElse(false)
-  def isLowNumSats = (for { v <- myVehicle; n <- v.numSats } yield { n < minNumSats }).getOrElse(false)
+  def isLowNumSats = (for { v <- myVehicle; n <- v.numSats } yield { v.hasHeartbeat && n < minNumSats }).getOrElse(false)
   def isWarning = isLowVolt || isLowBatPercent || isLowRssi || isLowNumSats
 
   /**
