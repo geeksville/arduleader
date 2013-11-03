64e0829675d1787250a7af276bc0777ef18a0fde
Kevin Hester <kevinh@geeksville.com>
2013-11-03 11:01:54 -0800

    make api actually do something

--- common/src/main/webapp/js/gcsapi.js
+++ common/src/main/webapp/js/gcsapi.js
@@ -41,13 +41,19 @@
 });
 	*/
 	
-  // **ListView class**: Our main app view.
-  var ListView = Backbone.View.extend({
+  var VehicleView = Backbone.View.extend({
     el: $('body'), // attaches `this.el` to an existing element.
-    
+
+    // This is where backbone binds DOM events to methods
+    events: {
+      'click button#refresh': 'refresh'
+    },
+
     // `initialize()`: Automatically called upon instantiation. Where you make all types of bindings, _excluding_ UI events, such as clicks, etc.
     initialize: function(){
-      _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+      _.bindAll(this, 'render', 'refresh'); // list all methods that use 'this' here
+
+       this.vehicle = new Vehicle();
 
        this.render(); // not all views are self-rendering. This one is.
     },
@@ -55,11 +61,18 @@
     // `render()`: Function in charge of rendering the entire view in `this.el`. Needs to be manually called by the user.
     render: function(){
       $(this.el).append("<ul> <li>hello world</li> </ul>");
+
+      $(this.el).append("<button id='refresh'>Refresh</button>");
+      $(this.el).append("<ul></ul>");
+    },
+
+    refresh: function(){
+      $('ul', this.el).append("<li>hello world "+ 4 +"</li>");
     }
   });
 
   // **listView instance**: Instantiate main app view.
-  var listView = new ListView();
+  var vehicleView = new VehicleView();
 })(jQuery);
 
 // <div style="float:left; margin-bottom:40px;"><img style="width:42px; margin-right:10px;" src="https://twitter.com/images/resources/twitter-bird-light-bgs.png"/></div> <div style="background:rgb(245,245,255); padding:10px;">Follow me on Twitter: <a target="_blank" href="http://twitter.com/r2r">@r2r</a> </div>
