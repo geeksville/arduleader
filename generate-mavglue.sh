d1d9de00edb06e420cb886192a1436de6a8fd4f8
Kevin Hester <kevinh@geeksville.com>
2014-02-15 23:01:08 -0800

    move to thirdparty so github doesn't think this is a java library

--- generate-mavglue.sh
+++ generate-mavglue.sh
@@ -1,1 +1,1 @@
-java -cp mavjava/org.mavlink.generator/bin:mavjava/org.mavlink.util/bin org.mavlink.generator.MAVLinkGenerator ../ardupilot/libraries/GCS_MAVLink/message_definitions/ardupilotmega.xml mavjava/org.mavlink.library/generated true false true true
+java -cp thirdparty/org.mavlink.generator/bin:mavjava/org.mavlink.util/bin org.mavlink.generator.MAVLinkGenerator ../ardupilot/libraries/GCS_MAVLink/message_definitions/ardupilotmega.xml thirdparty/org.mavlink.library/generated true false true true
