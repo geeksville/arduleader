51827a5ae8b94c0bb7068ae1f109d569e4691bab
Kevin Hester <kevinh@geeksville.com>
2014-03-10 18:16:20 -0700

    create scala native version of the GCSApi

---
+++ common/src/main/scala/com/geeksville/apiproxy/APIConstants.scala
@@ -0,0 +1,13 @@
+package com.geeksville.apiproxy
+
+object APIConstants {
+  /**
+   * The default world wide drone broker
+   */
+  val DEFAULT_SERVER = "localhost"
+
+  /**
+   * If using a raw TCP link to the server, use this port number
+   */
+  val DEFAULT_TCP_PORT = 5555
+}
