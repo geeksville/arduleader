68197426402156361e15f1efcb46318564e0a100
Kevin Hester <kevinh@geeksville.com>
2014-04-09 17:04:11 -1000

    end missions on live uploads

--- common/src/main/scala/com/geeksville/apiproxy/LiveUploader.scala
+++ common/src/main/scala/com/geeksville/apiproxy/LiveUploader.scala
@@ -6,11 +6,14 @@ import org.mavlink.messages.MAVLinkMessage
 import akka.actor.Props
 import akka.actor.ActorRefFactory
 import com.geeksville.mavlink.MavlinkReceiver
+import akka.actor.PoisonPill
+
+case object StopMissionAndExitMsg
 
 /**
  * Listens on the mavlink event bus for packets, and sends them to the server
  */
-class LiveUploader(override val isLive: Boolean) extends APIProxyActor with MavlinkReceiver {
+class LiveUploader(override val isLive: Boolean, host: String = APIConstants.DEFAULT_SERVER, port: Int = APIConstants.DEFAULT_TCP_PORT) extends APIProxyActor(host, port) with MavlinkReceiver {
   import APIProxyActor._
 
   self ! StartMissionMsg(false) // If we fail to close don't keep the mission
@@ -18,11 +21,14 @@ class LiveUploader(override val isLive: Boolean) extends APIProxyActor with Mavl
   MavlinkEventBus.subscribe(self, -1)
   // Messages will now be getting delivered to the proxy actor
 
-  override def postStop() {
-    self ! StopMissionMsg(true) // We completed the session normally - go ahead and keep the whole file
+  override def onReceive = ({
+    case StopMissionAndExitMsg =>
+      log.warning("Someone wants us to exit - close our mission")
+      // If somone asks us to exit, then _before_ exiting we will terminate any mission we were working on
+      self ! StopMissionMsg(true) // We completed the session normally - go ahead and keep the whole file
+      self ! PoisonPill // This request to stop will be handled normally
 
-    super.postStop()
-  }
+  }: Receive).orElse(super.onReceive)
 }
 
 object LiveUploader {
