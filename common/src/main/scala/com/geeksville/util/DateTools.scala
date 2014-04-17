0c22979ed8dd41d8bc497328c59e999b2c8e59b6
Kevin Hester <kevinh@geeksville.com>
2014-04-17 12:10:14 -1000

    date tools

---
+++ common/src/main/scala/com/geeksville/util/DateTools.scala
@@ -0,0 +1,16 @@
+package com.geeksville.util
+
+import java.util.TimeZone
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object DateTools {
+
+  val utcTZ = TimeZone.getTimeZone("UTC");
+  val df8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
+
+  df8601.setTimeZone(utcTZ)
+
+  def toISO8601(d: Date) = df8601.format(d)
+  def fromISO8601(d: String) = df8601.parse(d)
+}
