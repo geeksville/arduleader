b0e7288f5c5d53f26b630977c61648fe4399084e
Kevin Hester <kevinh@geeksville.com>
2014-05-19 11:42:50 -1000

    better analytics support

--- common/src/main/scala/com/geeksville/util/AnalyticsService.scala
+++ common/src/main/scala/com/geeksville/util/AnalyticsService.scala
@@ -5,6 +5,9 @@ package com.geeksville.util
  */
 trait AnalyticsAdapter {
   def reportException(msg: String, ex: Throwable)
+
+  // Add interesting information which will be attached _if_ we throw an error
+  def addBreadcrumb(key: String, value: String) {}
 }
 
 /**
@@ -27,6 +30,8 @@ object AnalyticsService {
    * Report an exception to our analytics
    */
   def reportException(msg: String, ex: Throwable) = handler.reportException(msg, ex)
+
+  def addBreadcrumb(key: String, value: String) = handler.addBreadcrumb(key, value)
 
   /**
    * Silently report any exceptions that occur in a block of code.
