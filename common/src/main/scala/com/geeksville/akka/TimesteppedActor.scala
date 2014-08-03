92292f91ef077fa3ded178ea685d6e77438c86ea
Kevin Hester <kevinh@geeksville.com>
2014-08-03 12:50:50 -0700

    create a mixin that can be used for timestamp based simulations

---
+++ common/src/main/scala/com/geeksville/akka/TimesteppedActor.scala
@@ -0,0 +1,39 @@
+package com.geeksville.akka
+
+import akka.actor.Actor
+import akka.actor.PoisonPill
+import scala.concurrent.duration._
+
+/**
+ * This is an Actor mixin that runs doNextStep periodically
+ */
+trait TimesteppedActor extends Actor {
+  import context._
+
+  private case object SimNext
+
+  def numPoints: Int
+  def interval: Double
+
+  protected var numRemaining = numPoints
+
+  private def scheduleNext() = context.system.scheduler.scheduleOnce(interval seconds, self, SimNext)
+
+  // Start our sim
+  scheduleNext()
+
+  protected def doNextStep(): Unit
+
+  abstract override def receive = ({
+    case SimNext =>
+      if (numRemaining == 0)
+        self ! PoisonPill
+      else {
+        doNextStep()
+
+        numRemaining -= 1
+        scheduleNext()
+      }
+  }: PartialFunction[Any, Unit]).orElse(super.receive)
+}
+
