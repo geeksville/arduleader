1540124e5e7625adde342cbd93d60866e76ed1cc
Kevin Hester <kevinh@geeksville.com>
2014-09-25 14:34:44 +0900

    fix error in iso8601 encoding - seconds were missing

--- common/src/main/scala/com/geeksville/util/DateTools.scala
+++ common/src/main/scala/com/geeksville/util/DateTools.scala
@@ -7,7 +7,7 @@ import java.util.Date
 object DateTools {
 
   val utcTZ = TimeZone.getTimeZone("UTC");
-  val df8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
+  val df8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
 
   df8601.setTimeZone(utcTZ)
 
